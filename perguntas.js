4385adf73939f3b9bfb49f37f271d9a85a794291
Assiz_Carlos <141234828+AssizCarlos1@users.noreply.github.com>
2024-10-24 11:15:11 -0300

    javascipt

--- perguntas.js
+++ perguntas.js
@@ -1,3 +1,35 @@
 criaPergunta(
+    "Programação",
+    "O Que e HTML",
+    "E uma Linguagem de marcaçao de hipertexto",
+)
 
+criaPergunta(
+    "Programação",
+    "O Que e HTML",
+    "E uma Linguagem de marcaçao de hipertexto",
+)
+
+criaPergunta(
+    "Programação",
+    "O Que e HTML",
+    "E uma Linguagem de marcaçao de hipertexto",
+)
+
+criaPergunta(
+    "Programação",
+    "O Que e HTML",
+    "E uma Linguagem de marcaçao de hipertexto",
+)
+
+criaPergunta(
+    "Programação",
+    "O Que e HTML",
+    "E uma Linguagem de marcaçao de hipertexto",
+)
+
+criaPergunta(
+    "Programação",
+    "O Que e HTML",
+    "E uma Linguagem de marcaçao de hipertexto",
 )
